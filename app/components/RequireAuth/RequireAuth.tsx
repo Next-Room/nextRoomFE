5ebd9a194330dd5c70843ac8c173e7e2a88d4556
lgrin-byte <lgrin02@naver.com>
2023-09-29 23:59:47 +0900

    feat: block mobile

--- app/components/RequireAuth/RequireAuth.tsx
+++ app/components/RequireAuth/RequireAuth.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Header from "@/components/common/Header/Header";
 import { useGetThemeList } from "@/queries/getThemeList";
 import { useCurrentTheme } from "@/components/atoms/currentTheme.atom";
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import * as S from "@/home/HomeView.styled";
 import { useIsLoggedInValue } from "@/components/atoms/account.atom";
 import MainDrawer from "@/components/common/Drawer/Drawer";
+import Mobile from "../Mobile/Mobile";
 
 interface RequireAuthProps {
   children: ReactNode;
@@ -19,7 +20,7 @@ function RequireAuth({
   const isLoggedIn = useIsLoggedInValue();
   const [currentTheme, setCurrentTheme] = useCurrentTheme();
   const router = useRouter();
-
+  const [isMobile, setIsMobile] = useState(false);
   const { data: categories = [] } = useGetThemeList();
   const props = { categories };
 
@@ -33,6 +34,22 @@ function RequireAuth({
       );
     }
   }, [categories, setCurrentTheme]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (
+        window.navigator.userAgent.match(/Android/i) ||
+        window.navigator.userAgent.match(/webOS/i) ||
+        window.navigator.userAgent.match(/iPhone/i) ||
+        window.navigator.userAgent.match(/iPad/i) ||
+        window.navigator.userAgent.match(/iPod/i) ||
+        window.navigator.userAgent.match(/BlackBerry/i) ||
+        window.navigator.userAgent.match(/Windows Phone/i)
+      ) {
+        setIsMobile(true);
+      }
+    }
+  }, []);
 
   useEffect(() => {
     if (isLoggedIn && currentTheme.length > 0) {
@@ -55,6 +72,9 @@ function RequireAuth({
       </S.Wrapper>
     );
   }
+
+  if (isMobile) return <Mobile />;
+
   // eslint-disable-next-line react/jsx-no-useless-fragment
   return <>{children}</>;
 }
