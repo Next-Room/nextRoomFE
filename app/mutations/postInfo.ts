df21e0c04719909e0c439da62f6f019619b08e65
lgrin-byte <lgrin02@naver.com>
2023-12-05 22:04:29 +0900

    feat: connect firebase

---
+++ app/mutations/postInfo.ts
@@ -0,0 +1,29 @@
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { FirebaseError } from "firebase/app";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { db } from "@/apis/firebase";
+
+interface CreatePostProps {
+  info: string;
+}
+
+export const postInfo = async ({ info }: CreatePostProps) => {
+  await addDoc(collection(db, "info"), {
+    info,
+    createdAt: Timestamp.now(),
+  });
+};
+
+export const usePostInfo = () => {
+  const queryClient = useQueryClient();
+  return useMutation(postInfo, {
+    onSuccess: () => {
+      console.log("실행");
+      
+      queryClient.invalidateQueries(["info"]);
+    },
+    onError: (error: FirebaseError) => {
+      console.error(error);
+    },
+  });
+};
