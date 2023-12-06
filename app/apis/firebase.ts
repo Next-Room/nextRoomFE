efcac7aadeb2a0686c3dd1616efee1ca1b31149e
lgrin-byte <lgrin02@naver.com>
2023-12-06 21:31:49 +0900

    chore: modify private key

--- app/apis/firebase.ts
+++ app/apis/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAquzRkRccQovbUZwg0CqLoXQOpfuxBJhU",
-  authDomain: "nextroom-1d5d9.firebaseapp.com",
-  projectId: "nextroom-1d5d9",
-  storageBucket: "nextroom-1d5d9.appspot.com",
-  messagingSenderId: "126156962007",
-  appId: "1:126156962007:web:9b3bfa79d63f1601d65892",
-  measurementId: "G-G7BVQSL1XE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
