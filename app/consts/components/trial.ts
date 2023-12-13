bf30518b75c901cbab0655995f41fe37bc0519dc
DABIN JEONG <41@b41.kr>
2023-12-13 23:26:00 +0900

    chore: add enter to trial_title

--- app/consts/components/trial.ts
+++ app/consts/components/trial.ts
@@ -1,4 +1,4 @@
-const TRIAL_TITLE = "사용 가이드와 계정을 보내드려요.";
+const TRIAL_TITLE = "사용 가이드와 \n계정을 보내드려요.";
 const CONTECT = "연락처를 남겨주시면 넥스트룸 팀이 준비해 드리겠습니다.";
 const EMAIL = "이메일 또는 연락처";
 const SUBSCRIPTION = "계정 신청하기";
