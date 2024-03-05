14b576efac46738dc8b76fac03d729b0fb5b1667
이경린 <66896175+lgrin-byte@users.noreply.github.com>
2024-03-05 17:50:36 +0900

    chore: modify description

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ import Clarity from "./apis/Clarity";
 
 export const metadata = {
   title: "NEXT ROOM",
-  description: "NEXT ROOM Admin",
+  description: "방탈출 운영이 편리해지고 테마 만족도가 올라가는 힌트폰 서비스 넥스트룸",
 };
 
 export default function RootLayout({
