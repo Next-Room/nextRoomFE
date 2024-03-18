da55dc5f4d9b04c4f980b3ceb77a9d7b52dbd077
DABIN JEONG <41@b41.kr>
2024-03-18 23:57:08 +0900

    chore: added og tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
+import { Metadata } from "next";
 import { Suspense } from "react";
+import Head from "next/head";
 
 import Recoil from "@/lib/recoil";
 import MuiProvider from "@/lib/muiProvider";
@@ -12,9 +14,10 @@ import Analytics from "./apis/Analytics";
 import ChannelTalk from "./apis/ChannelTalk";
 import Clarity from "./apis/Clarity";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "NEXT ROOM",
-  description: "방탈출 운영이 편리해지고 테마 만족도가 올라가는 힌트폰 서비스 넥스트룸",
+  description:
+    "방탈출 운영이 편리해지고 테마 만족도가 올라가는 힌트폰 서비스 넥스트룸",
 };
 
 export default function RootLayout({
@@ -24,6 +27,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
+      <Head>
+        <meta property="og:image" content="/images/og/og_1x.png" />
+        <meta property="og:image" content="/images/og/og_2x.png" />
+        <meta property="og:image" content="/images/og/og_3x.png" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://nextroom.co.kr" />
+      </Head>
       <body>
         <Suspense>
           <Analytics />
