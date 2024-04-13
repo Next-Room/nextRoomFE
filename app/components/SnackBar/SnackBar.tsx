b262d15f7a601732e79fa86e340c5bd798ac2b0f
lgrin-byte <lgrin02@naver.com>
2024-04-14 05:26:28 +0900

    style: add snackbar anchorOrigin

--- app/components/SnackBar/SnackBar.tsx
+++ app/components/SnackBar/SnackBar.tsx
@@ -4,10 +4,10 @@ import SnackBarView from "./SnackBarView";
 interface Props {
   open: boolean;
   ment: string;
+  vertical: "bottom" | "top";
+  horizontal: "left" | "center" | "right";
   handleClose: () => void;
 }
 export default function SnackBar(props: Props) {
-
-
   return <SnackBarView {...props} />;
 }
--- app/mutations/postSendMessage.ts
+++ app/mutations/postSendMessage.ts
@@ -1,4 +1,5 @@
 import { useSignUpWrite } from "@/components/atoms/signup.atom";
+import { useSnackBarWrite } from "@/components/atoms/snackBar.atom";
 import { apiClient } from "@/lib/reactQueryProvider";
 import { QUERY_KEY } from "@/queries/getHintList";
 import { ApiError, ApiResponse, MutationConfigOptions } from "@/types";
@@ -30,12 +31,17 @@ export const postSendMessage = async (req: Request) => {
 export const usePostSendMessage = (configOptions?: MutationConfigOptions) => {
   const queryClient = useQueryClient();
   const setSignUpState = useSignUpWrite();
+  const setSnackBar = useSnackBarWrite();
 
   const info = useMutation<Response, AxiosError<ApiError>, Request, void>({
     mutationKey: MUTATION_KEY,
     mutationFn: (req) => postSendMessage(req),
     ...configOptions?.options,
     onSuccess: (res, req) => {
+      setSnackBar({
+        isOpen: true,
+        message: `인증번호를 보냈습니다.`,
+      });
       queryClient.invalidateQueries(QUERY_KEY);
       setSignUpState({ level: 2, email: req.email, password: "" });
       // console.log("성공 시 실행")
@@ -45,11 +51,6 @@ export const usePostSendMessage = (configOptions?: MutationConfigOptions) => {
     },
     onError: (error) => {
       console.log(error);
-
-      // setSnackBar({
-      //   isOpen: true,
-      //   message: `${(error as any)?.response?.data?.message || error}`,
-      // });
     },
   });
 
