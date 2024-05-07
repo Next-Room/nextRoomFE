a067d473510ba01edd524444d67b613d6f63a350
DABIN JEONG <41@b41.kr>
2024-05-07 23:36:04 +0900

    chore: added dev environment

--- app/consts/env.ts
+++ app/consts/env.ts
@@ -1,4 +1,5 @@
-// eslint-disable-next-line import/prefer-default-export
 export const { NODE_ENV } = process.env;
 
-export const isDevMode = NODE_ENV === "development";
+export const isDevMode =
+  NODE_ENV === "development" ||
+  process.env.NEXT_PUBLIC_APP_ENVIRONMENT === "develop";
