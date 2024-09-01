2dbad5307119d4c85a798e5d74d6c0d9603d23f8
lgrin-byte <lgrin02@naver.com>
2024-09-02 01:26:55 +0900

    feat: theme 추가

--- app/style/theme.tsx
+++ app/style/theme.tsx
@@ -1,3 +1,5 @@
+import { css } from "styled-components";
+
 const fontSize = {
   xs: "12px",
   sm: "14px",
@@ -19,10 +21,112 @@ const color = {
   white70: "#ffffff70",
 };
 
+export const backGroundColor = {
+  main: "#131417",
+  sub1: "#1F2023",
+  sub2: "#3A3B3D",
+  white5: "#FFFFFF0D",
+  white8: "#FFFFFF1F",
+  white12: "#FFFFFF1F",
+  white20: "#FFFFFF33",
+  white50: "#FFFFFF80",
+  white70: "#FFFFFFB2",
+};
+
+
+export const title24M = css`
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+export const title20M = css`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+export const title20SB = css`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+export const title18M = css`
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const title18SB = css`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const title16SB = css`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+export const title16M = css`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const body16M = css`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const body16R = css`
+  font-size: 16px;
+  font-weight: 400;
+`;
+
+export const body14M = css`
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const body14R = css`
+  font-size: 14px;
+  font-weight: 400;
+`;
+export const body12M = css`
+  font-size: 12px;
+  font-weight: 500;
+`;
+
+export const body12R = css`
+  font-size: 12px;
+  font-weight: 400;
+`;
+
+export const caption12SB = css`
+  font-size: 12px;
+  font-weight: 600;
+`;
+export const caption12M = css`
+  font-size: 12px;
+  font-weight: 500;
+`;
+
 const theme = {
   fontSize,
   fontWeight,
   color,
+  backGroundColor,
+  title24M,
+  title20M,
+  title20SB,
+  title18SB,
+  title18M,
+  title16SB,
+  title16M,
+  body16M,
+  body16R,
+  body14M,
+  body14R,
+  body12M,
+  body12R,
+  caption12SB,
+  caption12M,
 };
 
 export default theme;
