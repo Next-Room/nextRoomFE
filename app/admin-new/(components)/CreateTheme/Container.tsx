66e42b28358c96678720d15b9276791c34908e21
anyl92 <yulim9592@gmail.com>
2024-10-01 18:41:08 +0900

    feat: 새 테마 추가 입력값 검증 로직 추가

--- app/admin-new/(components)/CreateTheme/Container.tsx
+++ app/admin-new/(components)/CreateTheme/Container.tsx
@@ -1,12 +1,11 @@
+import { FormEvent } from "react";
 import "../../(style)/createTheme.modules.sass";
-import CreateThemeTitle from "./CreateThemeTitle";
-import CreateThemeBody from "./CreateThemeBody";
-import CreateThemeAddButton from "./CreateThemeAddButton";
-
 import { useRouter } from "next/navigation";
 import { usePostTheme } from "@/mutations/postTheme";
 import { useCreateThemeValue } from "@/components/atoms/createTheme.atom";
-import { FormEvent } from "react";
+import CreateThemeTitle from "./CreateThemeTitle";
+import CreateThemeBody from "./CreateThemeBody";
+import CreateThemeAddButton from "./CreateThemeAddButton";
 
 export default function CreateTheme() {
   const router = useRouter();
@@ -15,11 +14,11 @@ export default function CreateTheme() {
 
   const handleKeyDownSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    const isDisabled = !(
-      createTheme.title &&
-      createTheme.timeLimit &&
-      createTheme.hintLimit
-    );
+    const isDisabled =
+      (!(createTheme.title && createTheme.timeLimit && createTheme.hintLimit) &&
+        createTheme.timeLimit < 1) ||
+      createTheme.timeLimit > 10000 ||
+      (createTheme.hintLimit && createTheme.hintLimit > 1000);
     if (isDisabled) {
       return;
     }
