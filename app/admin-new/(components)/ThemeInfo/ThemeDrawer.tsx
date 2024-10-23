3c873372bee25bb4bfe7a6250c15e029d895c4be
anyl92 <yulim9592@gmail.com>
2024-10-24 07:54:52 +0900

    feat: drawer 추가 기본 UI

--- app/admin-new/(components)/ThemeInfo/ThemeDrawer.tsx
+++ app/admin-new/(components)/ThemeInfo/ThemeDrawer.tsx
@@ -4,11 +4,23 @@ import Image from "next/image";
 import React from "react";
 
 export default function ThemeDrawer() {
-  const ImageProps = {
+  const XImageProps = {
     src: "/images/svg/icon_X.svg",
-    alt: "NEXT ROOM",
+    alt: "x_button",
     width: 28,
     height: 28,
+  };
+  const HintImageProps = {
+    src: "/images/svg/hint.svg",
+    alt: "hint_help_image",
+    width: 177,
+    height: 127,
+  };
+  const GalleryImageProps = {
+    src: "/images/svg/icon_gallery_white.svg",
+    alt: "gallery_image",
+    width: 11,
+    height: 11,
   };
 
   const codeTextFieldProps: ThemeInfoTextFieldType = {
@@ -21,35 +33,72 @@ export default function ThemeDrawer() {
     inputPlaceholder: "",
     checkErrorText: undefined,
   };
+  const rateTextFieldProps: ThemeInfoTextFieldType = {
+    id: "title",
+    tabIndex: 1,
+    title: "문제 진행률(%)",
+    content: "",
+    infoText: "",
+    inputType: "text",
+    inputPlaceholder: "",
+    checkErrorText: undefined,
+  };
+
   return (
     <div className="theme-drawer__container">
       <div className="theme-drawer__title">
         <span>힌트</span>
         <button type="button">
-          <Image {...ImageProps} />
+          <Image {...XImageProps} />
         </button>
       </div>
       <div className="theme-drawer__content">
         <div className="drawer-code">
           <ThemeTextField {...codeTextFieldProps} />
         </div>
-        <div>이미지</div>
+        <div className="drawer-image-subtext-section">
+          <div className="drawer-image-subtext">보여질 내용</div>
+        </div>
+        <div className="drawer-image-section">
+          <Image {...HintImageProps} />
+        </div>
         <div className="drawer-rate">
-          <div>문제 진행률</div>
-          <input type="text" placeholder="문제 진행률(%)" />
+          <div className="drawer-category-title">문제 진행률</div>
+          <ThemeTextField {...rateTextFieldProps} />
         </div>
         <div className="drawer-hint">
-          <div>힌트</div>
-          <button type="button">이미지 추가</button>
-          <textarea placeholder="힌트 내용을 입력해 주세요." />
+          <div className="drawer-category-title">
+            힌트
+            <button type="button">
+              <Image {...GalleryImageProps} />
+              이미지 추가
+            </button>
+          </div>
+          <div className="drawer-images">
+            <div className="drawer-image-box">image1</div>
+            <div className="drawer-image-box">image2</div>
+          </div>
+          <textarea
+            className="drawer-content-textarea"
+            placeholder="힌트 내용을 입력해 주세요."
+          />
         </div>
         <div className="drawer-answer">
-          <div>정답</div>
-          <button type="button">이미지 추가</button>
-          <textarea placeholder="정답 내용을 입력해 주세요." />
+          <div className="drawer-category-title">
+            정답
+            <button type="button">
+              <Image {...GalleryImageProps} />
+              이미지 추가
+            </button>
+          </div>
+          <textarea
+            className="drawer-content-textarea"
+            placeholder="정답 내용을 입력해 주세요."
+          />
         </div>
       </div>
       <div className="theme-drawer__footer">
+        <button type="submit">삭제하기</button>
         <button type="submit">추가하기</button>
       </div>
     </div>
